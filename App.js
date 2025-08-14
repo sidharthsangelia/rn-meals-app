8df1c00d95195543297f90f3f30a30f75f930364
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-08-14 16:38:48 +0530

    add react native navigation

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
- 
-    <SafeAreaView>
-      <StatusBar style="light"/>
-      <CategoriesScreen />
-    </SafeAreaView>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
