4e8ca24664d025491ce34fec8708ac7c54c46bf3
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-08-14 14:43:55 +0530

    initialised the project and added cateogiesScreen with categoryGridTile

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,33 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React from "react";
+
+export default function CategoryGridTile({ title }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+    return (
+        <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+    )
+}
+
+export default function CategoriesScreen() {
+  return (
+    <View>
+  
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+    </View>
+  );
+}
